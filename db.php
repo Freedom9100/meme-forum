a07bfb6f8de68c60a7a6fe40ab145842f87f4575
ilyabershev-lab <ilya.bershev@gmail.com>
2026-04-21 09:25:15 +0300

    added db

--- db.php
+++ db.php
@@ -1,14 +1,13 @@
 <?php
 
-$host = 'localhost';
-$dbname = 'meme_forum';
-$user = 'root';
-$pass = 'root';
-
+$host   = getenv('MYSQLHOST');
+$dbname = getenv('MYSQLDATABASE');
+$user   = getenv('MYSQLUSER');
+$pass   = getenv('MYSQLPASSWORD');
+$port   = getenv('MYSQLPORT') ?: 3306;
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die('Connection failed: ' . $e->getMessage());
 }
-?>
